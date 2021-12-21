5bc2b39134da07a2f7f1d5c58e265651dcb3482d
trinhphuonghoan <78306139+trinhphuonghoan@users.noreply.github.com>
2021-12-21 23:20:09 +0700

    Add files via upload

---
+++ routes/Todo.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { mongo } = require("mongoose");
+const router = express.Router();
+
+const {
+    getAllTodos,
+    createTodo,
+    getTodoById,
+    getTodo,
+    updateTodo,
+    deleteTodo,
+} = require("../controllers/Todo");
+
+router.get("/todos/", getAllTodos);
+
+router.post("/todo/create", createTodo);
+
+router.param("todoId", getTodoById);
+
+router.get("/todo/:todoId", getTodo);
+
+router.put("/todo/:todoId/update", updateTodo);
+
+router.delete("/todo/:todoId/delete", deleteTodo);
+
+module.exports = router;
